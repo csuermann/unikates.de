abc6ee5cc804db751bf4785e97f46dff3e989708
cornelius <cornelius.suermann@gmail.com>
2019-06-02 10:54:34 +0200

    initial commit

---
+++ items.js
@@ -0,0 +1,482 @@
+var items = [
+  {
+    description: 'Ring "Liaison"  Silber, Serpentin,Gold',
+    price: 290,
+    sold: false,
+    image: 'liaison.jpg'
+  },
+  {
+    description: 'Brosche/Anhänger  "mon hibou"  Messing,Stein,Gold,Silber  5x5cm',
+    price: 300,
+    sold: false,
+    image: 'mon-hibou.jpg'
+  },
+  {
+    description: 'Brosche "out of grace"',
+    price: 290,
+    sold: false,
+    image: 'out-of-grace.jpg'
+  },
+  {
+    description: 'Brosche "Poi-Le"    Corinthium Aes, Gold, Blattgold tauschiert',
+    price: 210,
+    sold: true,
+    image: 'BroscheChina.jpg'
+  },
+  {
+    description: 'Brosche "Growing"    Alpaka, Gold, Onyx    4x6 cm',
+    price: 240,
+    sold: false,
+    image: '00_B_008.jpg'
+  },
+  {
+    description: 'Ring "de profundis"   Gold, Silber, Chrysoberyll',
+    price: 330,
+    sold: false,
+    image: 'deProfundis.jpg'
+  },
+  {
+    description: 'Brosche "net"      feuerbehandeltes Kupfer, Silber    6,5x7,5 cm',
+    price: 160,
+    sold: true,
+    image: '00_B_021.jpg'
+  },
+  {
+    description: 'Brosche "lumi\u00e8re"',
+    price: 290,
+    sold: false,
+    image: 'Bergesgipfel.jpg'
+  },
+  {
+    description: 'Brosche "Fiole"  Silber, Gold, Chrysopras  4,5x5cm',
+    price: 230,
+    sold: false,
+    image: 'fiole.jpg'
+  },
+  {
+    description: 'Brosche "Sappho" Eisen, Messing, Silber, Gold Saphir  6x6cm',
+    price: 260,
+    sold: false,
+    image: 'Sappho.jpg'
+  },
+  {
+    description: 'Kette  "Sokrates"  Silber, Muschelmosaik, Karneol, Messing   2x5 cm',
+    price: 300,
+    sold: true,
+    image: '03_K_001.jpg'
+  },
+  {
+    description: 'Brosche "Undine" Messing, Keramik, Silber   4x7cm',
+    price: 160,
+    sold: false,
+    image: 'Undine.jpg'
+  },
+  {
+    description: 'cat-on-stripes',
+    price: 190,
+    sold: false,
+    image: '03_K_002a.jpg'
+  },
+  {
+    description: 'Brosche "TohuWa"  Silber, Eisen, Gold    7x5cm',
+    price: 260,
+    sold: false,
+    image: 'tohuwabohu.jpg'
+  },
+  {
+    description: 'Brosche "Goldfluss" Muschel, Feingold, Silber \u00d8 5cm',
+    price: 210,
+    sold: false,
+    image: 'Korallengold.jpg'
+  },
+  {
+    description: 'Brosche "Trust"   Messing, Alpaka, Elfenbein, Gold, Silber 4,8x5,5 cm',
+    price: 380,
+    sold: false,
+    image: 'trust.jpg'
+  },
+  {
+    description: 'Brosche / Kette "Onkel Liu"  Horn, Silber, Porzellan',
+    price: 230,
+    sold: true,
+    image: 'Onkel-Liu.jpg'
+  },
+  {
+    description: 'Brosche "Kreta"    Messing, Kupfer, Gold    7x5 cm',
+    price: 140,
+    sold: true,
+    image: '00_B_013.jpg'
+  },
+  {
+    description: 'Brosche "Hoachi"    Horn, Gold, Moosachat, Elfenbein   \u00d8 7 cm',
+    price: 200,
+    sold: true,
+    image: '01_B_017.jpg'
+  },
+  {
+    description: 'Brosche "Jeanne"   Messing, Elfenbein   6x2,5 cm',
+    price: 255,
+    sold: true,
+    image: 'Helmfrau.jpg'
+  },
+  {
+    description: 'Brosche "resting" Silber, Email, Gold 4x4,7cm',
+    price: 240,
+    sold: true,
+    image: 'resting.jpg'
+  },
+  {
+    description: 'Brosche "Venus M"   Silber, Elfenbein, Gold   4x4cm',
+    price: 370,
+    sold: true,
+    image: 'Venus_M.jpg'
+  },
+  {
+    description: 'Brosche "Bl\u00fctenstern"     Messing, Silber, Zirkon    4,5x4,5 cm',
+    price: 170,
+    sold: true,
+    image: '02_B_003.jpg'
+  },
+  {
+    description: 'Ring "Gemini"',
+    price: 280,
+    sold: true,
+    image: 'Gemini.jpg'
+  },
+  {
+    description: 'Ring "Rubya" Bronze, Granat, Silber',
+    price: 210,
+    sold: true,
+    image: 'Rubia.jpg'
+  },
+  {
+    description: 'Brosche "Golden Oak" Eiche, Blattgold, Pastellfarbe, Silber 4,5x7,5cm',
+    price: 165,
+    sold: true,
+    image: 'GoldBuntEiche.jpg'
+  },
+  {
+    description: 'Brosche "Herzrauch"  Messing, Gold, Rauchquarz 6x7,5cm',
+    price: 270,
+    sold: true,
+    image: 'Herzrauch.jpg'
+  },
+  {
+    description: 'Brosche  "Siam Te"    antikes Holz, Messing Gold',
+    price: 220,
+    sold: true,
+    image: 'Thai-Brosche.jpg'
+  },
+  {
+    description: 'Brosche "Asian Sunfield"    Kupfer, Messing, Gold    \u00d8 7 cm',
+    price: 150,
+    sold: false,
+    image: '00_B_015.jpg'
+  },
+  {
+    description: 'Brosche "Ilya"    Silber, Gold, Modelliermasse     7x4 cm',
+    price: 180,
+    sold: true,
+    image: '01_B_002a.jpg'
+  },
+  {
+    description: 'Ring "Spuren"  Silber, Messing auf Alpaka  2x2cm',
+    price: 190,
+    sold: true,
+    image: 'Spuren.jpg'
+  },
+  {
+    description: 'Kette "l\'eternit\u00e9"   Silber, Lava',
+    price: 220,
+    sold: true,
+    image: 'Steingesicht.jpg'
+  },
+  {
+    description: 'Brosche "Adio"   Silber (teilw. bemalt), Elfenbein, Gold   \u00d8 6cm',
+    price: 290,
+    sold: true,
+    image: 'Adio_1.jpg'
+  },
+  {
+    description: 'Brosche  "Kupferblatt"  Kupfer, Silber    4,5x5,5cm',
+    price: 135,
+    sold: true,
+    image: 'Kupferblatt.jpg'
+  },
+  {
+    description: 'Brosche "Aufbruch"',
+    price: 250,
+    sold: true,
+    image: 'Aufbruch.jpg'
+  },
+  {
+    description: 'Brosche "Schilf"   Horn, Stonium',
+    price: 150,
+    sold: true,
+    image: 'Hornbrosche-2006.jpg'
+  },
+  {
+    description: 'Brosche "flames"   Kupfer, Silber',
+    price: 140,
+    sold: true,
+    image: 'Flammenbrosche.jpg'
+  },
+  {
+    description: 'Kette & Amulett "Donatella"   roter Jaspis, Silber, Gold   \u00d8 4 cm',
+    price: 0,
+    sold: true,
+    image: '01_B_007.jpg'
+  },
+  {
+    description: 'Brosche  "Undine"   Silber, Elfenbein, Gold, Aquamarin    3x8 cm',
+    price: 320,
+    sold: true,
+    image: '03_B_001.jpg'
+  },
+  {
+    description: 'Ring  "Rosetta"     Silber, Messing     \u00d8  2,8 cm',
+    price: 140,
+    sold: true,
+    image: 'Rosetta.jpg'
+  },
+  {
+    description: '"Lurcher"',
+    price: 0,
+    sold: true,
+    image: 'Lurcher.jpg'
+  },
+  {
+    description: 'Brosche "Riff" Horn,  Silber,  Opal 4X6cm',
+    price: 160,
+    sold: true,
+    image: 'BroscheOpalriff.jpg'
+  },
+  {
+    description: 'Ring "Artemisia"    Aventurin, Silber, Bronze',
+    price: 190,
+    sold: true,
+    image: 'Artemisia.jpg'
+  },
+  {
+    description: 'Ring "Tri-Angle" Roh-Turmalin Silber Gold',
+    price: 290,
+    sold: true,
+    image: 'Turmalinring.jpg'
+  },
+  {
+    description: 'Brosche "Im Strahlenkranz"     (unverk\u00e4uflich)',
+    price: 0,
+    sold: false,
+    image: '00_B_001.jpg'
+  },
+  {
+    description: 'Ohrh\u00e4nger  "oscilla"   Aluminium, Lack, Silber, Disthen',
+    price: 80,
+    sold: true,
+    image: '03_O_002.jpg'
+  },
+  {
+    description: 'Ring "Eisenradt"     Zirkon, Silber, Eisen      \u00d8 3 cm',
+    price: 185,
+    sold: true,
+    image: 'Eisenring.jpg'
+  },
+  {
+    description: 'Brosche "ex oriente"   Silber, messing. Carneol, Karton',
+    price: 0,
+    sold: true,
+    image: 'ex-oriente.jpg'
+  },
+  {
+    description: 'Brosche "Erwarten"   Mesing, Silber, Elfenbein, Gold   4x5,5 cm',
+    price: 310,
+    sold: true,
+    image: '02_B_001.jpg'
+  },
+  {
+    description: 'Kette "drift" Treibholz, Messing, Kupfer 9x3,5cm',
+    price: 140,
+    sold: true,
+    image: 'driftwood.jpg'
+  },
+  {
+    description: 'Brosche  "Athora"   Horn, Silber, Gold, Feueropal     \u00d8 6 cm',
+    price: 270,
+    sold: true,
+    image: '02_B_004.jpg'
+  },
+  {
+    description: 'Brosche "equinocte"     Messing, Silber, Malachit    9 cm',
+    price: 145,
+    sold: true,
+    image: '02_B_002.jpg'
+  },
+  {
+    description: 'Brosche  "Diotima"    Messing, Keramik, Silber    \u00d8 5 cm',
+    price: 150,
+    sold: true,
+    image: '03_B_002.jpg'
+  },
+  {
+    description: 'Ohrh\u00e4nger "Korallenschlange" Metallknopf, Koralle, platiniertes Silber',
+    price: 0,
+    sold: true,
+    image: 'Korallenschlange.jpg'
+  },
+  {
+    description: 'Brosche "Tulipan"  Tiffany-Glas, Silber, Gold   4x4,5',
+    price: 180,
+    sold: true,
+    image: '01_B_018.jpg'
+  },
+  {
+    description: 'Brosche "Signum"      Silber, Messing      6 x 6 cm',
+    price: 210,
+    sold: true,
+    image: '00_B_019.jpg'
+  },
+  {
+    description: 'Kette "amber"      Bernstein, Messing',
+    price: 0,
+    sold: true,
+    image: '01_K_004.jpg'
+  },
+  {
+    description: 'Brosche "King II"     Silber, Steinnuss, Gold    3,5 x 5,5 cm',
+    price: 210,
+    sold: true,
+    image: '01_B_016.jpg'
+  },
+  {
+    description: 'Brosche "Bee Queen"  Silber, Messing',
+    price: 0,
+    sold: true,
+    image: '01_B_011.jpg'
+  },
+  {
+    description: 'Brosche "Hafen"    feuerbearbeitetes Messing, Silber, Aquamarin',
+    price: 0,
+    sold: true,
+    image: '00_B_006.jpg'
+  },
+  {
+    description: 'Brosche "Arabesque"       Silber, ge\u00e4tzt     \u00d8 5 cm',
+    price: 150,
+    sold: true,
+    image: '00_B_004.jpg'
+  },
+  {
+    description: 'Brosche "Bl\u00fctenstein"     Messing, Silber, Kupfer, Karneol',
+    price: 0,
+    sold: true,
+    image: '00_B_018.jpg'
+  },
+  {
+    description: 'Brosche "serenity"   Messing, Gold, Porzellan     \u00d8 7 cm',
+    price: 0,
+    sold: true,
+    image: '01_B_015.jpg'
+  },
+  {
+    description: 'Brosche "terra"    Messing, Silber, Landschaftsjaspis   5x6 cm',
+    price: 200,
+    sold: true,
+    image: '01_B_012.jpg'
+  },
+  {
+    description: 'Brosche "Golden Wings"     Messing, Silber, Opal',
+    price: 0,
+    sold: true,
+    image: '00_B_007.jpg'
+  },
+  {
+    description: 'Brosche "I-King"    Silber, Gold, Rubin, Knochen',
+    price: 0,
+    sold: true,
+    image: '00_B_002.jpg'
+  },
+  {
+    description: 'Brosche "Pagode"      Silber, Messing, Pyrit    5 x 6,5 cm',
+    price: 0,
+    sold: true,
+    image: '00_B_024.jpg'
+  },
+  {
+    description: 'Kette & Amulett "Donatella"   roter Jaspis, Silber, Gold   \u00d8 4 cm',
+    price: 0,
+    sold: true,
+    image: '00_B_003.jpg'
+  },
+  {
+    description: 'Brosche "Green Eagle"      Tombak, Gold, Chrysopas   5 x 6 cm',
+    price: 0,
+    sold: true,
+    image: '00_B_023.jpg'
+  },
+  {
+    description: 'Anh\u00e4nger "Schattengesicht"   Silber, Gold, Steinnuss, Messing',
+    price: 0,
+    sold: true,
+    image: '99_B_001a.jpg'
+  },
+  {
+    description: 'Brosche "Blumengarten"     Silber, Tombak',
+    price: 0,
+    sold: true,
+    image: '00_B_020.jpg'
+  },
+  {
+    description: 'Brosche "Angelus"    Silber, Gold, Bergkristall, Farbdruck',
+    price: 0,
+    sold: true,
+    image: '00_B_012.jpg'
+  },
+  {
+    description: 'Brosche "birdsEye"    Horn, Stein, Gold',
+    price: 0,
+    sold: true,
+    image: '01_B_013.jpg'
+  },
+  {
+    description: 'Kette "bouquet"    Onyx, Karneol, Silber    \u00d8 2,5 cm',
+    price: 0,
+    sold: true,
+    image: '01_K_006.jpg'
+  },
+  {
+    description: 'Brosche "penata"   Messing vergoldet, Holz, Turmalin, Gold 4 x 5 cm',
+    price: 0,
+    sold: true,
+    image: '01_B_008.jpg'
+  },
+  {
+    description: 'Brosche "River"    Silber, Lapislazuli    \u00d8 5,5 cm',
+    price: 0,
+    sold: true,
+    image: '00_B_011.jpg'
+  },
+  {
+    description: 'Collier  "eternity"    Meteorit, Gold',
+    price: 0,
+    sold: true,
+    image: '02_K_002.jpg'
+  },
+  {
+    description: 'Ring  "anesuna"    Messing, geschw\u00e4rztes Silber',
+    price: 0,
+    sold: true,
+    image: 'xx_R_001.jpg'
+  },
+  {
+    description: 'Ohrh\u00e4nger   "UW"   geschw\u00e4rztes Silber, Gold, Koralle',
+    price: 0,
+    sold: true,
+    image: '03_O_001.jpg'
+  },
+  {
+    description: 'Brosche "onwards"    Messing',
+    price: 0,
+    sold: true,
+    image: '01_B_004.jpg'
+  }
+]
